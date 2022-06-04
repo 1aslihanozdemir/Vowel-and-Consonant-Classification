1a08fdd0cb68b6cf5f7cebd18e24f6194ca236e5
1aslihanozdemir <aslihanozdemir97@gmail.com>
2022-06-04 18:03:48 +0300

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,21 @@
+import string
+
+
+Alphabet = "aeiouAEIOUbcdfghjklmnprstvyzqxBCDFGHJKLMNPRSTVYZXQ"
+vowels ="aeiouAEIOU"
+
+
+vowels_list=[]
+consonant_list=[]
+
+
+
+for i in Alphabet:
+    if i in vowels:
+        vowels_list.append(i)
+    else:
+        consonant_list.append(i)
+
+    
+print("vovels: ",vowels_list)
+print("consonants: ",consonant_list)
